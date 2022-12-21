733d9989573ae8dabb3c0d68f2efe80f1c7b9fca
Inderpal Singh <inderpalsingh6763@gmail.com>
2022-12-21 03:01:07 -0500

    Add Edit Delete functionality working properly

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const addBtns = document.querySelectorAll(".addBtn");
+const addBtns = document.querySelectorAll(".btn-primary");
 
 const addFormAccounts = document.querySelector(".addFormAccounts");
 const addFormDebts = document.querySelector(".addFormDebts");
 const addFormExpenses = document.querySelector(".addFormExpenses");
 const addFormIncomes = document.querySelector(".addFormIncomes");
+
+const editFormAccounts = document.querySelector(".editFormAccounts");
+const editFormDebts = document.querySelector(".editFormDebts");
+const editFormExpenses = document.querySelector(".editFormExpenses");
+const editFormIncomes = document.querySelector(".editFormIncomes");
 
 // storage for articles we add:
 // sections > articles
@@ -22,17 +27,24 @@ addBtns.forEach((btn) => {
 
 		switch (btnClassName) {
 			case "addAccountsBtn":
+				// clearInputValues();
 				addFormAccounts.classList.toggle("addFormHide");
 		}
 	});
 });
 
-// main inputs
+// main add form inputs
 const addAccountName = document.getElementById("addAccountName");
 const addAccountAmt = document.getElementById("addAccountAmt");
 
+function clearInputValues() {
+	addAccountName.value = "";
+	addAccountAmt.value = "";
+}
+
+// add form buttons
 const addFormBtn = document.querySelector(".addFormBtn"); // for every single add button on add forms
-const cancelFormBtn = document.querySelector(".cancelFormBtn"); // for every single cancel button on add forms
+const addCancelFormBtn = document.querySelector(".addCancelFormBtn"); // for every single cancel button on add forms
 
 // adding article for every section
 addFormBtn.addEventListener("click", (e) => {
@@ -43,16 +55,18 @@ addFormBtn.addEventListener("click", (e) => {
 		case addFormAccounts:
 			const isNotEmptyForm = checkEmptyAddForm(addAccountName, addAccountAmt);
 
-			let newArticleObj = {
-				id: Math.random().toString(36).slice(2, 9),
-				articleName: addAccountName.value,
-				articleAmt: addAccountAmt.value,
-				checked: false,
-			};
-
 			if (isNotEmptyForm) {
-				addArticle("add", "account", newArticleObj);
-				addFormAccounts.before(createArticle("Account", addAccountName, addAccountAmt));
+				const articleId = Math.random().toString(36).slice(2, 9);
+				const articleEl = createArticle(articleId, "Account", addAccountName, addAccountAmt);
+
+				let newArticleObj = {
+					id: articleId,
+					articleName: addAccountName.value,
+					articleAmt: addAccountAmt.value,
+					checked: false,
+				};
+				articleReducer("add", "account", newArticleObj);
+				addFormAccounts.before(articleEl);
 			} else {
 				setTimeout(() => {
 					addAccountName.style.outline = "none";
@@ -83,7 +97,7 @@ function checkEmptyAddForm(nameInput, amtInput) {
 }
 
 // specific function for creating article
-function createArticle(sectionClassName, _articleName, _articleAmt) {
+function createArticle(id, sectionClassName, _articleName, _articleAmt) {
 	const article = document.createElement("article");
 	article.className = sectionClassName.toLowerCase(); // account can be replaced with a function parameter
 
@@ -99,10 +113,12 @@ function createArticle(sectionClassName, _articleName, _articleAmt) {
 
 	const secondDiv = document.createElement("div");
 	const editSpan = document.createElement("span"); // need to work on these controls also
-	editSpan.className = `edit${sectionClassName}Btn`;
+	editSpan.className = `editArticleBtn`;
+	editSpan.id = `edit${id}`;
 	editSpan.textContent = "E";
 	const delSpan = document.createElement("span"); // need to work on these controls also
-	delSpan.className = `del${sectionClassName}Btn`;
+	delSpan.className = `delArticleBtn`;
+	delSpan.id = `del${id}`;
 	delSpan.textContent = "D";
 
 	secondDiv.appendChild(editSpan);
@@ -124,7 +140,7 @@ function createArticle(sectionClassName, _articleName, _articleAmt) {
 }
 
 // toggling the add forms (cancel button)
-cancelFormBtn.addEventListener("click", (e) => {
+addCancelFormBtn.addEventListener("click", (e) => {
 	//check which form is clicked on
 	const addFormParentNode = e.target.parentNode.parentNode;
 
@@ -144,10 +160,119 @@ next steps that we need to take:
 - can delete them easily also
 */
 
-function articleReducer(action, section, article) {
-	switch (action) {
-		case "add":
-			const _section = state[section];
-			_section.push(article);
+// selected main for bubbling and event propagation
+const main = document.querySelector("main");
+
+let currentEditingArticleId;
+
+// editing & deleting articles
+main.addEventListener("click", (e) => {
+	if (e.target.closest(".delArticleBtn")) {
+		const articleId = e.target.getAttribute("id");
+		const parentArticle = e.target.parentNode.parentNode.parentNode;
+		const articleSection = parentArticle.className;
+		articleReducer("delete", articleSection, { id: articleId });
+		parentArticle.remove();
+	}
+	if (e.target.closest(".editArticleBtn")) {
+		const parentArticle = e.target.parentNode.parentNode.parentNode;
+		const articleSection = parentArticle.className;
+		const articleId = e.target.getAttribute("id");
+		currentEditingArticleId = articleId;
+		const article = state[articleSection].find((_article) => `edit${_article.id}` === articleId);
+		toggleEditForm(articleSection, article.articleName, article.articleAmt);
+	}
+});
+
+// main edit form inputs
+const editAccountName = document.getElementById("editAccountName");
+const editAccountAmt = document.getElementById("editAccountAmt");
+
+// need to make a new edit form in html and then make it toggle and then click edit btn and then change the state
+function toggleEditForm(section, name, amt) {
+	switch (section) {
+		case "account":
+			editFormAccounts.classList.toggle("editFormHide");
+			editAccountName.value = name;
+			editAccountAmt.value = amt;
 	}
 }
+
+// edit form buttons
+const editFormBtn = document.querySelector(".editFormBtn"); // for every single add button on edit forms
+const editCancelFormBtn = document.querySelector(".editCancelFormBtn"); // for every single cancel button on edit forms
+
+// editing article for every section
+editFormBtn.addEventListener("click", (e) => {
+	//check which form is clicked on
+	const editFormParentNode = e.target.parentNode.parentNode;
+
+	switch (editFormParentNode) {
+		case editFormAccounts:
+			const isNotEmptyForm = checkEmptyAddForm(editAccountName, editAccountAmt);
+
+			if (isNotEmptyForm) {
+				articleReducer("edit", "account", {
+					id: currentEditingArticleId,
+					editAccountName,
+					editAccountAmt,
+				});
+				updateUI(currentEditingArticleId, editAccountName.value, editAccountAmt.value);
+				editFormAccounts.classList.toggle("editFormHide");
+			} else {
+				setTimeout(() => {
+					editAccountName.style.outline = "none";
+					editAccountAmt.style.outline = "none";
+				}, 2000);
+			}
+			break;
+	}
+});
+
+// toggling the add forms (cancel button)
+editCancelFormBtn.addEventListener("click", (e) => {
+	//check which form is clicked on
+	const editFormParentNode = e.target.parentNode.parentNode;
+
+	switch (editFormParentNode) {
+		case editFormAccounts:
+			editAccountName.value = "";
+			editAccountAmt.value = "";
+			editFormAccounts.classList.toggle("editFormHide");
+	}
+});
+
+// updating UI
+function updateUI(id, name, amt) {
+	const editedArticle = document.getElementById(id).parentNode.parentNode.parentNode;
+	const editedArticleH3 = editedArticle.querySelector("h3");
+	const editedArticleP = editedArticle.querySelector("p");
+    editedArticleH3.textContent = name;
+    editedArticleP.textContent = `$${amt}`;
+}
+
+function articleReducer(action, section, article) {
+	let _section;
+	switch (action) {
+		case "add":
+			_section = state[section];
+			_section.push(article);
+			break;
+		case "delete":
+			state[section] = state[section].filter((_article) => `del${_article.id}` !== article.id);
+			break;
+		case "edit":
+			state[section] = state[section].map((_article) => {
+				if (`del${_article.id}` !== article.id) {
+					return {
+						..._article,
+						articleName: editAccountName.value,
+						articleAmt: editAccountAmt.value,
+					};
+				}
+				return _article;
+			});
+	}
+}
+
+// calculating net amount
